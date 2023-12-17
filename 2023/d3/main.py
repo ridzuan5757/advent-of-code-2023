4e397d7427ef7c5214eddefad6babf4a85445b09
ridzuan5757 <ridzuan5757@protonmail.com>
2023-12-17 14:34:03 +0800

    d3 py rewrite

--- 2023/d3/main.py
+++ 2023/d3/main.py
@@ -1,103 +1,59 @@
 import sys
-from pprint import pprint
 
 
-class Coords:
-    def __init__(self, val,  x, y):
-        self.val = val
-        self.x = x
-        self.y = y
-        return
+def check_adjacent(f, coords):
+    candidates = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1)
+    ]
 
-    def __repr__(self):
-        return self.__str__()
+    print("number grid check", f[coords[0]][coords[1]], coords)
 
-    def __str__(self):
-        return f"Val:{self.val} x:{self.x} y:{self.y}"
+    for candidate in candidates:
+        y, x = tuple(map(lambda x, y: x + y, coords, candidate))
 
-    def get_adj_tl(self):
-        return {"x": self.x - 1, "y": self.y - 1}
-
-    def get_adj_tc(self):
-        return {"x": self.x, "y": self.y - 1}
-
-    def get_adj_tr(self):
-        return {"x": self.x + 1, "y": self.y - 1}
-
-    def get_adj_cl(self):
-        return {"x": self.x - 1, "y": self.y}
-
-    def get_adj_cr(self):
-        return {"x": self.x + 1, "y": self.y}
-
-    def get_adj_bl(self):
-        return {"x": self.x - 1, "y": self.y + 1}
-
-    def get_adj_bc(self):
-        return {"x": self.x, "y": self.y + 1}
-
-    def get_adj_br(self):
-        return {"x": self.x + 1, "y": self.y + 1}
-
-    def get_adj(self):
-        return [
-            self.get_adj_tl(),
-            self.get_adj_tc(),
-            self.get_adj_tr(),
-            self.get_adj_cl(),
-            self.get_adj_cr(),
-            self.get_adj_bl(),
-            self.get_adj_bc(),
-            self.get_adj_br()
-        ]
-
-    def get_adj_x(self):
-        return [coords["x"] for coords in self.get_adj()]
-
-    def get_adj_y(self):
-        return [coords["y"] for coords in self.get_adj()]
+        try:
+            if not f[y][x].isnumeric() and f[y][x] != ".":
+                return True
+        except Exception:
+            pass
+    return False
 
 
-class Part:
-    def __init__(self, partval):
-        self.partval = partval
-        self.coords_list = []
-        return
+sum = []
+is_real_part = False
 
-    def insert(self, coords):
-        self.coords_list.append(coords)
-        return
-
-    def length(self):
-        return len(self.partval)
-
-    def value(self):
-        return int(self.partval)
-
-    def __repr__(self):
-        return f"Part: {self.partval} list: {self.coords_list}"
-
-
-partsCollection = []
-symbolCollection = []
 with open(sys.argv[1]) as f:
-    for y, line in enumerate(f):
-        line = line.rstrip("\r\n")
-        parts = [part for part in line.split(".") if part.isdigit()]
 
-        # part check
-        for partstring in parts:
-            part_obj = Part(partstring)
-            location = line.find(partstring)
+    lines = f.readlines()
+    grid_data = [line.strip().split()[0] for line in lines]
 
-            # append every part coordinates inside part
-            for idx, part in enumerate(partstring):
-                part_obj.insert(Coords(part, idx + location, y))
-            partsCollection.append(part_obj)
+    for y, line in enumerate(grid_data):
 
-        # symbol check_call
-        for x, char in enumerate(line):
-            if char != ".":
-                if not char.isdigit():
-                    # print(f"char: {char} x:{x} y:{y}")
-                    symbolCollection.append(Coords(char, x, y))
+        line_length = len(line)
+        part_number = ""
+
+        for x, char in enumerate(line[0]):
+
+            if char.isnumeric():
+
+                part_number += char
+                is_adjacent = check_adjacent(grid_data, (y, x))
+
+                print(char, "adjacent: ", is_adjacent)
+                is_real_part |= is_adjacent
+
+            else:
+                if is_real_part and part_number != "":
+                    print(part_number)
+                    sum.append(int(part_number))
+
+                part_number = ""
+
+print(sum)
